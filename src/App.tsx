15c36f513271d48da5b38a3f8b35020688c4f71b
OlgaGrishchenko <olg.grishchenko@gmail.com>
2022-12-27 09:01:33 +0300

    add component UserName

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import FormContainer from "./Components/FormContainer";
 import Input from "./Components/Input";
+import UserName from "./Components/UserName";
 import NewPassword from "./Pages/NewPassword";
 
 function App() {
   return (
     <div className="App">
-      <NewPassword />
+      <UserName username={"Olga_Grishchenko"}/>
+
     </div>
   );
 }
